e303eb0886e1a316835bc1990badecc77019d947
asmakovets <asmakovets@lanit.ru>
2019-09-24 10:10:21 +0300

    firs comment after recreate

--- Test1/Test1/AppDelegate.h
+++ Test1/Test1/AppDelegate.h
@@ -4,7 +4,7 @@
 //
 //  Created by Михаил Асмаковец on 24/09/2019.
 //  Copyright © 2019 Михаил Асмаковец. All rights reserved.
-//
+// Test 1 first comment after recreating
 
 #import <UIKit/UIKit.h>
 
