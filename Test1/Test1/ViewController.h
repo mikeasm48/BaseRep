13fdfec53f09b539935bfb0d108f160818f6c9c0
asmakovets <asmakovets@lanit.ru>
2019-09-24 10:08:33 +0300

    re-create Test1

---
+++ Test1/Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by Михаил Асмаковец on 24/09/2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test1/Test1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test1
+//
+//  Created by Михаил Асмаковец on 24/09/2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
