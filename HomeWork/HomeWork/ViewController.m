29d96df84f6c10c44dcb8f88208741756c2d6c64
asmakovets <asmakovets@lanit.ru>
2019-09-26 09:25:16 +0300

    Home Work first

---
+++ HomeWork/HomeWork/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HomeWork
+//
+//  Created by Михаил Асмаковец on 26/09/2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
