b501cdf713cf73c2e48f43c2470777b6e3b2ccb1
asmakovets <asmakovets@lanit.ru>
2019-10-30 16:58:38 +0300

    [HomeWorkDashBoard] - добавлен drag&drop в рамках одного stage

--- HomeWorkDashBoard/HomeWorkDashBoard/StageView.swift
+++ HomeWorkDashBoard/HomeWorkDashBoard/StageView.swift
@@ -13,12 +13,13 @@ class StageView: NSObject, UICollectionViewDelegate, UICollectionViewDataSource,
     //var delegate: ISSBottomMenuDelegate?
     
     var collectionView: UICollectionView!
-    var isToggled: Bool = false;
-    let cellHeight: CGFloat = 50.0
+    //var isToggled: Bool = false;
+    //TODO изменяяем на 20 - полэкрана, ставим 50 - и на весь экран по вышине! разобраться
+    let cellHeight: CGFloat = 100.0
     let cellSpacing: CGFloat = 10.0
     let cellSectionSpacing: CGFloat = 15.0
     
-    let itemsArray: Array<String> = {
+    var itemsArray: Array<String> = {
         let array: Array<String> = ["TASK-1", "TASK-2", "TASK-3","TASK-4", "TASK-5", "TASK-6", "TASK-7", "TASK-8", "TASK-9", "TASK-10","TASK-1", "TASK-2", "TASK-3","TASK-4", "TASK-5", "TASK-6", "TASK-7", "TASK-8", "TASK-9", "TASK-10"]
         return array
     }()
@@ -34,6 +35,11 @@ class StageView: NSObject, UICollectionViewDelegate, UICollectionViewDataSource,
         collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: flowLayout)
         collectionView.delegate = self
         collectionView.dataSource = self
+        //Drag&Drop
+        collectionView.dragInteractionEnabled = true
+        collectionView.dragDelegate = self;
+        collectionView.dropDelegate = self;
+        
         collectionView.backgroundColor = .lightGray
         collectionView.register(TaskItemCollectionViewCell.self, forCellWithReuseIdentifier: "menuCell")
         
@@ -48,16 +54,6 @@ class StageView: NSObject, UICollectionViewDelegate, UICollectionViewDataSource,
             let fitHeight = self.collectionViewHeight > window.frame.size.height ? window.frame.size.height : self.collectionViewHeight
             collectionView.frame = CGRect(x: x, y: y, width: viewWidth, height: fitHeight - 100)
         }
-    //TODO добавить заголовок, который не скролится
-//        let textLabel = UILabel()
-//        textLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
-//        textLabel.textAlignment = .center
-//        textLabel.textColor = .blue
-//        textLabel.text = "ToDo"
-//        collectionView.addSubview(textLabel)
-//        let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: viewWidth, height: 10))
-//        navBar.backgroundColor = collectionView.backgroundColor
-//        collectionView.addSubview(navBar)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -68,7 +64,7 @@ class StageView: NSObject, UICollectionViewDelegate, UICollectionViewDataSource,
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuCell", for: indexPath) as! TaskItemCollectionViewCell
         cell.backgroundColor = .yellow;
         cell.textLabel.text = itemsArray[indexPath.row]
-        cell.picture.image = UIImage(named: itemsArray[indexPath.row])
+        //cell.picture.image = UIImage(named: itemsArray[indexPath.row])
         return cell
     }
     
@@ -92,11 +88,90 @@ class StageView: NSObject, UICollectionViewDelegate, UICollectionViewDataSource,
         return UIEdgeInsets(top: cellSectionSpacing, left: cellSectionSpacing, bottom: cellSectionSpacing, right: cellSectionSpacing)
     }
     
-//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-//        cell.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-//        UIView.animate(withDuration: 0.5){
-//            cell.transform = .identity
-//        }
-//    }
+    //For Drag&Drop
+    private func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView)
+    {
+        let items = coordinator.items
+        if items.count == 1, let item = items.first, let sourceIndexPath = item.sourceIndexPath
+        {
+            var dIndexPath = destinationIndexPath
+            if dIndexPath.row >= collectionView.numberOfItems(inSection: 0)
+            {
+                dIndexPath.row = collectionView.numberOfItems(inSection: 0) - 1
+            }
+            collectionView.performBatchUpdates({
+                self.itemsArray.remove(at: sourceIndexPath.row)
+                self.itemsArray.insert(item.dragItem.localObject as! String, at: dIndexPath.row)
+                collectionView.deleteItems(at: [sourceIndexPath])
+                collectionView.insertItems(at: [dIndexPath])
+            })
+            coordinator.drop(items.first!.dragItem, toItemAt: dIndexPath)
+        }
+    }
 
+}
+
+//Drag & Drop
+//Drag
+extension StageView: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let item = self.itemsArray[indexPath.row]
+        let itemProvider = NSItemProvider(object: item as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = item
+        
+        return [dragItem]
+    }
+    
+    //func collectionView(_ collectionView: UICollectionView, dragPreviewParametersForItemAt indexPath: IndexPath) -> UIDragPreviewParameters?
+    //{
+    //        let previewParameters = UIDragPreviewParameters()
+    //        previewParameters.visiblePath = UIBezierPath(rect: CGRect(x: 25, y: 25, width: 120, height: 120))
+    //        return previewParameters
+    //}
+}
+
+
+//Drop
+extension StageView : UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool
+    {
+        return session.canLoadObjects(ofClass: NSString.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal
+    {
+        if collectionView.hasActiveDrag
+        {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        else
+        {
+            return UICollectionViewDropProposal(operation: .forbidden)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator)
+    {
+        let destinationIndexPath: IndexPath
+        if let indexPath = coordinator.destinationIndexPath
+        {
+            destinationIndexPath = indexPath
+        }
+        else
+        {
+            let section = collectionView.numberOfSections - 1
+            let row = collectionView.numberOfItems(inSection: section)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+        
+        switch coordinator.proposal.operation
+        {
+        case .move:
+            self.reorderItems(coordinator: coordinator, destinationIndexPath:destinationIndexPath, collectionView: collectionView)
+            break
+        default:
+            return
+        }
+    }
 }
--- HomeWorkDashBoard/HomeWorkDashBoard/ViewController.swift
+++ HomeWorkDashBoard/HomeWorkDashBoard/ViewController.swift
@@ -8,35 +8,35 @@
 
 import UIKit
 
+struct StageHolder {
+    var stageName : String
+    var stage : StageView?
+}
+
 class ViewController: UIViewController, UIScrollViewDelegate {
-    
-    let scrollView = UIScrollView()
-    let toDoStageView = StageView.init()
-    let inProgressStageView = StageView.init()
-    let reviewStageView = StageView.init()
-    let inQAStageView = StageView.init()
-    let doneStageView = StageView.init()
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
+
+    var stages = [StageHolder(stageName: "ToDo", stage: StageView.init()),
+    StageHolder(stageName: "InProgress", stage: StageView.init()),
+    StageHolder(stageName: "Done", stage: StageView.init())]
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // Do any additional setup after loading the view.
-        
-        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+        let screenWidth:Int = Int(self.view.frame.width)
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 50))
         navigationBar.backgroundColor = .lightText
         let addTaskButton = UINavigationItem(title: "Добавить задачу")
         navigationBar.items = [addTaskButton]
         self.view.addSubview(navigationBar)
         //Init Stages
         let stageViewUpperBound = navigationBar.frame.height + 10
-        let stageViewWidth = 350
-        toDoStageView.setPosition(x: 10, y: stageViewUpperBound)
-        inProgressStageView.setPosition(x: CGFloat(10 + stageViewWidth), y: stageViewUpperBound)
-        reviewStageView.setPosition(x: CGFloat(10 + stageViewWidth * 2), y: stageViewUpperBound)
-        inQAStageView.setPosition(x: CGFloat(10 + stageViewWidth * 3), y: stageViewUpperBound)
-        doneStageView.setPosition(x: CGFloat(10 + stageViewWidth * 4), y: stageViewUpperBound)
-    
-
+        let stageViewWidth = (screenWidth + (stages.count))/stages.count
+        var stageCounter = 0
+        for stageHolder in stages {
+            let deltaX = CGFloat(10 + stageViewWidth * stageCounter)
+            stageHolder.stage?.setPosition(x: deltaX, y: stageViewUpperBound)
+            stageCounter += 1
+        }
     }
 
 
