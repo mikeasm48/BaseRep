091fb2a616e02d99a34ff78c9d08d959eaf15b62
asmakovets <asmakovets@lanit.ru>
2019-11-15 15:39:04 +0300

    [HWUrlSession] - дополнительный коммит

---
+++ HWUrlSession/HWUrlSession/Models/ImageModel.swift
@@ -0,0 +1,19 @@
+//
+//  ImageViewModel.swift
+//  HWUrlSession
+//
+//  Created by Михаил Асмаковец on 15.11.2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+import UIKit
+
+struct ImageModel {
+    let path: String
+    let description: String
+}
+
+struct ImageViewModel {
+    let description: String
+    let image: UIImage
+}
---
+++ HWUrlSession/HWUrlSession/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  HWUrlSession
+//
+//  Created by Михаил Асмаковец on 15.11.2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let tableView = UITableView()
+    var images: [ImageViewModel] = []
+    let reuseId = "UITableViewCellreuseId"
+    let interactor: InteractorInput
+    
+    init(interactor: InteractorInput) {
+        self.interactor = interactor
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("Метод не реализован")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseId)
+        tableView.dataSource = self
+        loadImage()
+        search(by: "cat")
+    }
+    
+    private func loadImage() {
+        let imagePath = "http://s16.stc.all.kpcdn.net/share/i/12/11048313/inx960x640.jpg"
+        interactor.loadImage(at: imagePath) { [weak self] image in
+            if let image = image {
+                let model = ImageViewModel(description: "Тестовая картинка", image: image)
+                self?.images = [model]
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    private func search(by searchString: String) {
+        interactor.loadImageList(by: searchString) { [weak self] models in
+            //            self.images = models
+            self?.loadImages(with: models)
+        }
+    }
+    
+    private func loadImages(with models: [ImageModel]) {
+        let models = models.suffix(10)
+        
+        let group = DispatchGroup()
+        for model in models {
+            group.enter()
+            interactor.loadImage(at: model.path) { [weak self] image in
+                guard let image = image else {
+                    group.leave()
+                    return
+                }
+                let viewModel = ImageViewModel(description: model.description,
+                                               image: image)
+                self?.images.append(viewModel)
+                group.leave()
+            }
+            
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath)
+        let model = images[indexPath.row]
+        cell.imageView?.image = model.image
+        cell.textLabel?.text = model.description
+        return cell
+    }
+}
+
